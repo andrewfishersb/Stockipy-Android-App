4aeeed8871ff4573fa3c942298829cbfad1da151
Andrew Fisher <AndrewFishersb@gmail.com>
2016-11-18 10:16:43 -0800

    let user display their shopping list on the shopping list page

--- app/src/main/java/fisher/andrew/stockipy/ShoppingList.java
+++ app/src/main/java/fisher/andrew/stockipy/ShoppingList.java
@@ -2,12 +2,34 @@ package fisher.andrew.stockipy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ShoppingList extends AppCompatActivity {
-
+    @Bind(R.id.shoppingListView) ListView mShoppingListView;
+    private ArrayList<String> shoppingList = new ArrayList<String>(Arrays.asList(
+            "Bell Peppers",
+            "Pork",
+            "Bread Crumbs",
+            "Onion",
+            "Sour Cream",
+            "Potatoes"
+    ));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, shoppingList);
+        mShoppingListView.setAdapter(adapter);
+
+
     }
 }
