7b32c5c57fba1118fa96a8dd4bbafd57b1cb8539
Andrew Fisher <AndrewFishersb@gmail.com>
2016-11-18 17:03:03 -0800

    user can add and display an item on their favorites activity, it wont yet link and is still repeats but those are problems are another day

--- app/src/main/java/fisher/andrew/stockipy/YourRecipes.java
+++ app/src/main/java/fisher/andrew/stockipy/YourRecipes.java
@@ -1,13 +1,29 @@
 package fisher.andrew.stockipy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class YourRecipes extends AppCompatActivity {
-
+    @Bind(R.id.favoriteRecipesListView) ListView mFavoriteRecipesListView;
+    private ArrayList<String> favoriteRecipes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_recipes);
+        ButterKnife.bind(this);
+        Intent intent = getIntent();
+        favoriteRecipes = intent.getStringArrayListExtra("favorites");
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,favoriteRecipes);
+        mFavoriteRecipesListView.setAdapter(adapter);
+
     }
 }
+//favoriteRecipesListView
