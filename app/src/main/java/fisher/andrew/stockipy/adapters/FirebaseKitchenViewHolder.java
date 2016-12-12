f4b407651469721160db254340fe442e3626c068
Andrew Fisher <AndrewFishersb@gmail.com>
2016-12-11 22:10:25 -0800

    get rid of useless code that cause a click listener for the kitchen view holder

--- app/src/main/java/fisher/andrew/stockipy/adapters/FirebaseKitchenViewHolder.java
+++ app/src/main/java/fisher/andrew/stockipy/adapters/FirebaseKitchenViewHolder.java
@@ -1,31 +1,18 @@
 package fisher.andrew.stockipy.adapters;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import org.parceler.Parcels;
-
-import java.util.ArrayList;
-
-import fisher.andrew.stockipy.Constants;
 import fisher.andrew.stockipy.R;
 import fisher.andrew.stockipy.models.Food;
-import fisher.andrew.stockipy.ui.kitchen.KitchenActivity;
 
 /**
  * Created by andrewfisher on 12/6/16.
  */
 
-public class FirebaseKitchenViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class FirebaseKitchenViewHolder extends RecyclerView.ViewHolder {
     View mView;
     Context mContext;
 
@@ -33,7 +20,6 @@ public class FirebaseKitchenViewHolder extends RecyclerView.ViewHolder implement
         super(itemView);
         mView = itemView;
         mContext = itemView.getContext();
-        itemView.setOnClickListener(this);// this isnt need right?
     }
 
     public void bindKitchen(Food item){
@@ -42,32 +28,6 @@ public class FirebaseKitchenViewHolder extends RecyclerView.ViewHolder implement
 
     }
 
-    @Override
-    public void onClick(View v){
-        final ArrayList<Food> items = new ArrayList<>();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_KITCHEN);
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    items.add(snapshot.getValue(Food.class));
-                }
-//dont need below?
-                int itemPosition = getLayoutPosition();
-
-                Intent intent = new Intent(mContext, KitchenActivity.class);
-                intent.putExtra("position",itemPosition+"");
-                intent.putExtra("items", Parcels.wrap(items));
-
-                mContext.startActivity(intent);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }
 
 
 }
